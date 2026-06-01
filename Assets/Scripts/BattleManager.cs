7539907c43c079b4a1e05032669a50b7dc9c1ca3
Leesung257 <dltjdwls2579@gmail.com>
2026-06-01 10:13:53 +0900

    Update BattleManager.cs

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,5 +1,8 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
@@ -11,17 +14,128 @@ public class BattleManager : MonoBehaviour
     public TMP_Text enemyHpText;
     public TMP_Text resultText;
 
+    public TMP_Text deckCountText;
+    public TMP_Text handCountText;
+    public TMP_Text discardCountText;
+
+    public Button cardButton1;
+    public Button cardButton2;
+    public Button cardButton3;
+
+    List<string> deck = new List<string>();
+    List<string> hand = new List<string>();
+    List<string> discardPile = new List<string>();
+
+    string card1;
+    string card2;
+    string card3;
 
     void Start()
     {
         resultText.text = "";
+
+        MakeDeck();
+        ShuffleDeck();
+        DrawCards();
+
         UpdateUI();
     }
-
-    // Update is called once per frame
-    public void UseAttackCard()
+    void MakeDeck()
     {
-        enemyHp -= 10;
+        deck.Clear();
+
+        deck.Add("공격 카드");
+        deck.Add("공격 카드");
+        deck.Add("공격 카드");
+        deck.Add("공격 카드");
+        deck.Add("강공격 카드");
+        deck.Add("회복 카드");
+    }
+
+    void ShuffleDeck()
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            int randomIndex = Random.Range(i, deck.Count);
+
+            string temp = deck[i];
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+
+    void DrawCards()
+    {
+        hand.Clear();
+
+        DrawOneCard();
+        DrawOneCard();
+        DrawOneCard();
+
+        cardButton1.GetComponentInChildren<TMP_Text>().text = hand[0];
+        cardButton2.GetComponentInChildren<TMP_Text>().text = hand[1];
+        cardButton3.GetComponentInChildren<TMP_Text>().text = hand[2];
+    }
+
+    void DrawOneCard()
+    {
+        if (deck.Count == 0)
+        {
+            deck.AddRange(discardPile);
+            discardPile.Clear();
+            ShuffleDeck();
+        }
+
+        string card = deck[0];
+        deck.RemoveAt(0);
+        hand.Add(card);
+    }
+
+
+    public void UseCard1()
+    {
+        UseCard(0);
+    }
+
+    public void UseCard2()
+    {
+        UseCard(1);
+    }
+
+    public void UseCard3()
+    {
+        UseCard(2);
+    }
+
+    void UseCard(int handIndex)
+    {
+        if (enemyHp <= 0 || playerHp <= 0)
+        {
+            return;
+        }
+
+        string cardName = hand[handIndex];
+
+        if(cardName=="공격 카드")
+        {
+            enemyHp -= 10;
+        }
+        else if(cardName=="강공격 카드")
+        {
+            enemyHp -= 20;
+        }
+        else if(cardName=="회복 카드")
+        {
+            playerHp += 15;
+
+            if (playerHp > 100)
+            {
+                playerHp = 100;
+            }
+        }
+
+        DiscardHand();
+
         CheckEnemyDead();
 
         if (enemyHp > 0)
@@ -29,33 +143,18 @@ public class BattleManager : MonoBehaviour
             EnemyAttack();
         }
 
+        DrawCards();
         UpdateUI();
     }
 
-    public void UseStrongAttackCard()
+    void DiscardHand()
     {
-        enemyHp -= 20;
-        CheckEnemyDead();
-
-        if (enemyHp > 0)
+        for (int i = 0; i < hand.Count; i++)
         {
-            EnemyAttack();
+            discardPile.Add(hand[i]);
         }
 
-        UpdateUI();
-    }
-
-    public void UseHealCard()
-    {
-        playerHp += 15;
-
-        if (playerHp > 100)
-        {
-            playerHp = 100;
-        }
-
-        EnemyAttack();
-        UpdateUI();
+        hand.Clear();
     }
 
     void EnemyAttack()
@@ -81,5 +180,9 @@ public class BattleManager : MonoBehaviour
     {
         playerHpText.text = "플레이어 HP : " + playerHp;
         enemyHpText.text = "슬라임 HP : " + enemyHp;
+
+        deckCountText.text = "덱 : " + deck.Count;
+        handCountText.text = "손패 : " + hand.Count;
+        discardCountText.text = "버림더미 : " + discardPile.Count;
     }
 }
